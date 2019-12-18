e492ebffbe4665fc17de0a7d26492d726e275d68
github username <github email address>
2019-12-18 13:27:15 -0600

    Done.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -6,8 +6,48 @@ class ApplicationController < Sinatra::Base
   configure do
     set :public_folder, 'public'
     set :views, 'app/views'
+    set :method_override, true
   end
 
   get '/' do
+    redirect to "/articles"
   end
+
+  get '/articles' do
+    @articles = Article.all
+    erb:'index'
+  end
+
+  get '/articles/new' do
+    erb:'new'
+  end
+
+  post '/articles' do
+    # binding.pry
+    @article = Article.create(title: params[:title], content: params[:content])
+    redirect to "/articles/#{ @article.id }"
+  end
+
+  get '/articles/:id' do
+    @article = Article.find_by(id: params[:id])
+    erb:'show'
+  end
+
+  get '/articles/:id/edit' do
+    @article = Article.find_by(id: params[:id])
+    erb:'edit'
+  end
+
+  patch '/articles/:id' do
+    @article = Article.find_by(id: params[:id])
+    @article.update(title: params[:title], content: params[:content])
+    redirect to "/articles/#{ @article.id }"
+  end
+
+  delete '/articles/:id' do
+    @article = Article.find_by(id: params[:id])
+    @article.delete
+    redirect to "/articles"
+  end
+
 end
